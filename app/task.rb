a45abe90e3e4c4d8c9711807f0e87464bd166d5f
Isaac Seymour <i.seymour@oxon.org>
2019-10-14 18:06:49 +0100

    Someone call the :cop: :siren:

--- app/task.rb
+++ app/task.rb
@@ -3,9 +3,9 @@
 Task = Value.new(:radius, :central_point, :max_distance) do
   def call(io)
     User.parse_all(io)
-      .select { |user| distance(user) <= max_distance }
-      .sort_by(&:id)
-      .map { |user| "#{user.name} (#{user.id})" }
+        .select { |user| distance(user) <= max_distance }
+        .sort_by(&:id)
+        .map { |user| "#{user.name} (#{user.id})" }
   end
 
   private
