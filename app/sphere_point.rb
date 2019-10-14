cb3aa220ebb483d6b556408f6bc5b7e55f40b4ff
Isaac Seymour <i.seymour@oxon.org>
2019-10-14 17:11:59 +0100

    Implement central angle between two points

--- app/sphere_point.rb
+++ app/sphere_point.rb
@@ -11,7 +11,28 @@ SpherePoint = Value.new(:latitude, :longitude) do
     degrees_to_rad(longitude)
   end
 
+  def central_angle(other)
+    # https://en.wikipedia.org/wiki/Great-circle_distance#Formulae
+    # Using the basic formula - I hope you're on a 64-bit CPU!
+    Math.acos(
+      sin_latitudes(other) +
+      (cos_latitudes(other) * cos_longitude_diff(other))
+    )
+  end
+
   private
+
+  def sin_latitudes(other)
+    Math.sin(latitude_radians) * Math.sin(other.latitude_radians)
+  end
+
+  def cos_latitudes(other)
+    Math.cos(latitude_radians) * Math.cos(other.latitude_radians)
+  end
+
+  def cos_longitude_diff(other)
+    Math.cos((longitude_radians - other.longitude_radians).abs)
+  end
 
   def degrees_to_rad(degrees)
     degrees * Math::PI / 180
--- spec/sphere_point_spec.rb
+++ spec/sphere_point_spec.rb
@@ -11,4 +11,14 @@ RSpec.describe SpherePoint do
     expect(point.latitude_radians).to be_within(1e-10).of(0.8992959819686)
     expect(point.longitude_radians).to be_within(1e-10).of(-0.00148179151626)
   end
+
+  it 'calculates the central angle' do
+    other_point = described_class.with(latitude: 51.526042,
+                                       longitude: -0.0914257)
+
+    # Result here taken from
+    # https://www.vcalc.com/wiki/MichaelBartmess/Great+Circle+Central+Angle
+    expect(point.central_angle(other_point))
+      .to be_within(1e-10).of(0.0000709243930982)
+  end
 end
